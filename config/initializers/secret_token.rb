4faec1e9a561ba822574636d859c30e5c5a783dd
joshua sweetkind-singer <sweetkindsinger@gmail.com>
2015-02-17 09:48:16 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JssNotes::Application.config.secret_token = 'b17c18502bfb2230cf87929db1309adfeec8f203d163a481241c09ea2b0a0fc17af7ab498da70f539df991bdde9ca90cc1e1d0ae9d2c0154c1ea92d30eb04042'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+JssNotes::Application.config.session_store :cookie_store, key: '_jss_notes_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# JssNotes::Application.config.session_store :active_record_store
