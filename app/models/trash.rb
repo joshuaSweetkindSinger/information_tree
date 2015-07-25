eb0ad9ee26c7c29a1b853493e85b0d557a02af2e
joshua sweetkind-singer <sweetkindsinger@gmail.com>
2015-07-24 22:20:14 -0700

    fix bug in delete-old-nodes

--- app/models/trash.rb
+++ app/models/trash.rb
@@ -33,7 +33,7 @@ class Trash < Node
 
   # Delete all nodes in the trash that were put there longer than DAYS_TO_KEEP_TRASHED_NODE days ago.
   def delete_old_nodes
-    children.where('created_at < ?', Time.now - DAYS_TO_KEEP_TRASHED_NODE.day).each do |node|
+    children.where('updated_at < ?', Time.now - DAYS_TO_KEEP_TRASHED_NODE.day).each do |node|
       node.destroy
     end
   end
