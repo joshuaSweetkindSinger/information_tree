0fbf2f762f7eb6c2c492bc8db9df03557485d6d1
joshua sweetkind-singer <sweetkindsinger@gmail.com>
2015-08-12 13:02:33 -0700

    incremental work on visited node list

---
+++ app/assets/javascripts/ui_visited_node_list.js
@@ -0,0 +1,14 @@
+/*
+This file defines class VistedNodeList, which is the toplevel dom element container on the
+client side for the nodes that have been visited by the user.
+ */
+// ========================================================================
+//                   Visited Node List
+// ========================================================================
+var VisitedNodeList = defCustomTag('visited-node-list', HTMLElement);
+
+VisitedNodeList.prototype.init = function() {
+  return this
+};
+
+
